1dce07687b65a2d812071705ab99c3470008978e
Gonzalo Murias <gonzalomurias@gmail.com>
2021-11-01 00:03:57 -0300

    quinto commit, creacion de clases producto, marca, talle y categoria + arquitectura en capas, negocio y dominio

---
+++ E-COMMERCE/Dominio/Talle.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    public class Talle
+    {
+        public int ID { get; set; }
+        public int Numero { get; set; }
+        public bool Estado { get; set; }
+
+    }
+}
---
+++ E-COMMERCE/Negocio/ProductoNegocio.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dominio;
+
+namespace Negocio
+{
+    class ProductoNegocio
+    {
+        public List<Producto> listar()
+
+        {
+            List<Producto> lista = new List<Producto>();
+            AccesoDatos datos = new AccesoDatos();
+
+            try
+            {
+                datos.setearConsulta("Select P.ID, C.Nombre as Categoria, M.Nombre as Marca, T.Numero as Talle, P.Nombre, P.Descripcion, P.Precio, P.UrlImagen, P.Stock from Productos as P, Marcas as M, Categorias as C where P.IdMarca = M.Id and P.IdCategoria = C.Id and P.IdTalle = T.Id");
+                datos.ejecutarLectura();
+
+                while (datos.Lector.Read())
+                {
+                    Producto aux = new Producto();
+
+                    aux.ID = (int)datos.Lector["Id"];
+
+                    aux.Marca = new Marca();
+                    aux.Marca.ID = (int)datos.Lector["IdMarca"];
+                    aux.Marca.Nombre = (string)datos.Lector["Marca"];
+
+                    aux.Categoria = new Categoria();
+                    aux.Categoria.ID = (int)datos.Lector["IdCategoria"];
+                    aux.Categoria.Nombre = (string)datos.Lector["Categoria"];
+
+                    aux.Talle = new Talle();
+                    aux.Talle.ID = (int)datos.Lector["IdTalle"];
+                    aux.Talle.Numero = (int)datos.Lector["Talle"];
+
+                    if (!(datos.Lector["Nombre"] is DBNull))
+                        aux.Nombre = (string)datos.Lector["Nombre"];
+
+                    if (!(datos.Lector["Descripcion"] is DBNull))
+                        aux.Descripcion = (string)datos.Lector["Descripcion"];
+
+                    if (!(datos.Lector["UrlImagen"] is DBNull))
+                        aux.UrlImagen = (string)datos.Lector["UrlImagen"];
+
+                    if (!(datos.Lector["Precio"] is DBNull))
+                        aux.Precio = (decimal)datos.Lector["Precio"];
+
+                    if (!(datos.Lector["Stock"] is DBNull))
+                        aux.Stock = (int)datos.Lector["Stock"];
+
+                    lista.Add(aux);
+                }
+
+                return lista;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
+        }
+    }
+}
